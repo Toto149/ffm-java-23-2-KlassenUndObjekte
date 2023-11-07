9c281984949d070de06f3073181b90f5f784be0d
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-11-07 11:53:05 +0100

    add object and use "beschleunigt" on it.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Auto auto = new Auto();
         auto.startet();
+        Auto auto1 = new Auto(23);
+        auto1.beschleunigt();
+        System.out.println(auto1.geschwindigkeit);
+
     }
 }
