90cbdfa3e539e480f56269ae8aec6dd12215424e
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-11-07 11:34:20 +0100

    add method "startet" to the class Auto

--- src/main/java/org/example/Auto.java
+++ src/main/java/org/example/Auto.java
@@ -1,2 +1,35 @@
-package org.example;public class Auto {
+package org.example;
+
+public class Auto {
+
+    String marke;
+    String modell;
+    String farbe;
+    int baujahr;
+    int gefahreneKilometer;
+    int maximaleGeschwindigkeit;
+    int anzahlReifen;
+
+    int ps;
+
+    public Auto(){
+
+    }
+
+    public Auto(String marke, String modell, String farbe, int baujahr, int gefahreneKilometer,
+                int maximaleGeschwindigkeit, int anzahlReifen, int ps){
+        this.marke = marke;
+        this.modell = modell;
+        this.farbe = farbe;
+        this.baujahr = baujahr;
+        this.gefahreneKilometer = gefahreneKilometer;
+        this.maximaleGeschwindigkeit = maximaleGeschwindigkeit;
+        this.anzahlReifen = anzahlReifen;
+    }
+
+    public void startet(){
+        System.out.println("Dieses Auto ist gestartet und kann jetzt losfahren");
+    }
+
+
 }
